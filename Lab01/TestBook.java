345f1be38d96227abd6b1ca04e62a06d94f896a3
SaharaT024 <5835512024@psu.ac.th>
2018-02-09 04:27:34 +0700

    Update Lab01

--- Lab01/TestBook.java
+++ Lab01/TestBook.java
@@ -4,13 +4,24 @@ public class TestBook
 { 
   public static void main(String args[])
   {
-    Book mybook1;
-    mybook1 = new Book();
-    mybook1.settitle("OOP");
-    mybook1.setnumberofpage(100);
-    mybook1.setauthorName("John Cena");
-    System.out.println("Book1 Title is "+mybook1.gettitle() +
-                       ",Number of Page is "+mybook1.getnumberofpage() +
-                       ",Author Name is "+mybook1.getauthorName());
+    Book mybook1,mybook2;
+    mybook1 = new Book("Java Application",300,"JK");
+    mybook2 = new Book("Java Application",300,"JK");
+    System.out.println("Book1 Title is "+mybook1.gettitle() +",Number of Pages is "
+                        +mybook1.getnumberofpage() +",Author Name is " 
+                        +mybook1.getauthorName());
+    System.out.println("Book2 Title is "+mybook2.gettitle() +",Number of Pages is "
+                        +mybook2.getnumberofpage() +",Author Name is " 
+                        +mybook2.getauthorName());
+    System.out.println("Are Book1 and Book2 is True or False ? --> "+ mybook1.isSameBook(mybook2));
+    mybook2.setnumberofpage(400);
+    System.out.println("Book1 Title is"+mybook1.gettitle() +",Number of Pages is "
+                        +mybook1.getnumberofpage() +",Author Name is " 
+                        +mybook1.getauthorName());
+    System.out.println("Book2 Title is"+mybook2.gettitle() +",Number of Pages is "
+                        +mybook2.getnumberofpage() +",Author Name is " 
+                        +mybook2.getauthorName());
+    System.out.println("Are Book1 and Book2 is True or False ? --> "+ mybook1.isSameBook(mybook2));
   }
 }
+
